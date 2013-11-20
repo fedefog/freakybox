6eea9ac41921ab4b5200bccc003404636214c04d
Pablo Cornehl <seinoxygen@hotmail.com>
2013-11-20 15:39:11 -0300

    Borrado archivos viejos inutilizados.

--- templates/novedad.php
+++
@@ -1,1 +0,0 @@
-Archivo: novedad.php
--- templates/novedad/ver.php
+++
@@ -1,6 +0,0 @@
-Archivo: ver.php<br/>
-<?php
-echo "Segmento 3: ".$uri->segment(3)."<br/>";
-echo "Segmento 4: ".$uri->segment(4)."<br/>";
-echo $_GET["getvalue"];
-?>
