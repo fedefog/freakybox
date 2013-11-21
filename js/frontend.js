a85cd9576f96dfd005b1e2618127ba62fd17496a
Pablo Cornehl <seinoxygen@hotmail.com>
2013-11-21 16:56:14 -0300

    Agregadas las funcioens de crear Team.

--- js/frontend.js
+++ js/frontend.js
@@ -5,12 +5,15 @@ var Core = (function() {
 	// Keep the team id of the current user.
 	var _team;
 	
+	var _date = new Date();
+	
     function Core(){};	
 			
 	Core.prototype.init = function(url) {
 		_host = url;
 		_socket = io.connect(_host);
 		
+		_socket.on('createTeam', createTeam);
 		_socket.on('createProject', createProject);
     };
 	
@@ -18,9 +21,40 @@ var Core = (function() {
         _socket.emit(event, data);
     };
 	
+	Core.prototype.createTeam = function(data) {
+		var uid = _date.getTime();
+		
+		data.uid = uid;
+	
+		createTeam(data);
+        _socket.emit('createTeam', data);
+    };
+	
 	Core.prototype.createProject = function(data) {
 		createProject(data);
         _socket.emit('createProject', data);
+    };
+	
+	var createTeam = function(data) {
+		var html = '<div id="team-'+data.uid+'" class="team-wrapper">';
+		html = html + '<div class="team-name"><a title="Team Name" href="#">'+data.nombre+'</a></div>';
+		html = html + '<div class="team-content">';	
+		html = html + '<div class="team-people">';
+		for (var key in data.members) {
+			var user = data.members[key];
+			html = html + '<a class="" href="">';
+			html = html + '<img title="'+user.nombre+'" alt="User" src="http://www.gravatar.com/avatar/'+user.avatar+'?d=identicon&amp;s=22">';
+			html = html + '</a>';
+		}
+		html = html + '</div><!-- / team people -->';
+		html = html + '<span class="clearfix"></span>';
+		html = html + '<a role="menuitem" href="#projectModal" data-team="'+data.id+'" data-toggle="modal" class="add-project">PROJECT</a>';
+		html = html + '<ul class="projects-team" id="team-'+data.id+'">';
+		html = html + '</ul><!-- / projects team -->';
+		html = html + '</div><!-- / team-content -->';
+		html = html +'</div><!-- / team-wrapper -->';
+		
+        $('div.workspace-wrapper').append(html);
     };
 	
 	var createProject = function(data) {
--- js/server.js
+++ js/server.js
@@ -15,11 +15,66 @@ var clients = {};
 
 io.sockets.on('connection', function (socket) {
 		
-	socket.on('connect', function(data) {
+	socket.on('connect', function(data){
 		clients[data.nickname] = socket.id;
 	});
+	
+	socket.on('createTeam', function(data){
+		//@TODO: Cambiar el 1 por el fk de usuario creador.
+		var usuario_id = 1;
 		
-	socket.on('createProject', function (data) {
+		var team = {
+			fk_usuario_id: usuario_id,
+			team_nombre: data.nombre,
+			team_privado: data.privado
+		};
+				
+		connection.query('INSERT INTO team SET ?', team, function(err, result) {
+			if (err) throw err;
+			
+			var team_id = result.insertId;			
+			
+			linkTeamUser(team_id, usuario_id)
+			
+			for (var key in data.members) {
+				var user = data.members[key];
+				
+				var follower_id = user.id;
+				
+				if(follower_id == 0){
+					connection.query('SELECT usuario_id FROM usuario WHERE LOWER(usuario_email) = ?', user.email, function(err, results, fields) {
+						if (err) throw err;
+						// Si el email existe lo vinculamos directamente
+						var numRows = results.length;
+						if(numRows > 0){
+							for (var i in results) {
+								var user = results[i];
+								linkTeamUser(team_id, user.usuario_id);
+							}
+						}
+						else{
+							var usuario = {
+								usuario_email: user.email
+							};
+							connection.query('INSERT INTO usuario SET ?', usuario, function(err, result) {
+								if (err) throw err;
+								linkTeamUser(team_id, result.insertId);
+								linkUserUser(usuario_id, result.insertId);
+							});
+						}
+					});				
+				}
+				else{
+					linkTeamUser(team_id, follower_id);
+				}			
+			}
+			
+			data.id = team_id;
+			socket.broadcast.emit('createTeam', data);
+		});
+	});
+		
+	socket.on('createProject', function(data){
 		if(data.privado == 0){
 			socket.broadcast.emit('createProject', data);
 		}
@@ -46,4 +101,36 @@ io.sockets.on('connection', function (socket) {
 		});
     });
 });
+
+/*
+* Vincular el Team con el Usuario.
+*
+* Se uso este metodo porque las llamadas son asyncronas a la db y asi se reutiliza codigo.
+*/
+var linkTeamUser = function(team_id, usuario_id) {
+	var rel_teamusuario = {
+		fk_team_id: team_id,
+		fk_usuario_id: usuario_id
+	};
+				
+	connection.query('INSERT INTO rel_teamusuario SET ?', rel_teamusuario, function(err, result) {
+		if (err) throw err;
+	});
+}
+
+/*
+* Vincular el Usuario con Usuario.
+*
+* Se utiliza mas que nada cuando invitas por primera vez al usuario.
+*/
+var linkUserUser = function(usuario_id, contacto_id) {
+	var rel_usuariousuario = {
+		fk_usuario_id: usuario_id,
+		fk_contacto_id: contacto_id
+	};
+				
+	connection.query('INSERT INTO rel_usuariousuario SET ?', rel_usuariousuario, function(err, result) {
+		if (err) throw err;
+	});
+}
 console.log('Server running');
